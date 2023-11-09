99d2f413feefd2b750e11871c21a12d1def7732d
thuanbkk20 <thuan17082002@gmail.com>
2023-11-09 19:51:21 +0700

    chef helper onboard

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { OnboardFlow } from 'react-native-onboard';
+import { Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <OnboardFlow
+      pages={[
+        {
+          title: 'Unleash Your Inner Chef',
+          subtitle: 'From ingredients to meal - your delicious journey begins here',
+          imageUri: Image.resolveAssetSource(require('./app/assets/onboarding1.png')).uri,
+        },
+        {
+          title: 'Scan & Savor',
+          subtitle: 'Scan your ingredients and get your recipes effortlessly',
+          imageUri: Image.resolveAssetSource(require('./app/assets/onboarding2.png')).uri,
+        },
+        {
+          title: 'Snap, Save, Share',
+          subtitle: 'Discover, bookmark and share delicious recipes with ChefHelper',
+          imageUri: Image.resolveAssetSource(require('./app/assets/onboarding3.png')).uri
+        }
+      ]}
+      type={'fullscreen'}
+    />
   );
 }
 
